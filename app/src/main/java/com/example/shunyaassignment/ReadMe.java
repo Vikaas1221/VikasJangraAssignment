1515ba1394a8c438644e6e669f0410c9cb65cf74
pc <vj11701590@gmail.com>
2020-09-11 23:14:03 +0530

    Description will be added

---
+++ app/src/main/java/com/example/shunyaassignment/ReadMe.java
@@ -0,0 +1,22 @@
+package com.example.shunyaassignment;
+
+public class ReadMe
+{
+    /*
+    Coding language->Java
+    Software->Android Studio
+    Architecture->Model View View Model(MVVM)
+    Databases->Cloud Firestore
+
+    In this app user can upload different types documents such as .pdf,.txt,.xlx,.ppt,.docx and that file will be stored in Firebase storage
+    and the name and download url link in Cloud firestore
+
+   1) First user has to select the type of document he wants to upload and then user has to click on the upload button after selecting
+   the document from local storage
+   2) If user wants to download the document then he has to click on retrive button and it will show all the documents to the user and
+   then user can just clcik on the download button in the list of items
+
+   Documents are downloaded using Download manager.
+   MVVM architecture is used so that project can be maintained easily
+     */
+}
